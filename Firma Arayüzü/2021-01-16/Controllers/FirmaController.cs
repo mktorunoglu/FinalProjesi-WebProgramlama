c694646055760107be52b8a9f5c3bc6c50840049
Mehmet Kamil Torunoğlu <32582511+mktorunoglu@users.noreply.github.com>
2021-01-16 05:02:28 +0300

    Firma Arayüzü

---
+++ Firma Arayüzü/2021-01-16/Controllers/FirmaController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web.Mvc;
+using LSYSweb.Models.Entity;
+
+namespace LSYSweb.Controllers
+{
+    public class FirmaController : Controller
+    {
+        // GET: Firma
+        LSYSEntities db = new LSYSEntities();
+
+        public ViewResult Urunler()
+        {
+            var degerler = db.TBL_URUN.ToList();
+            return View(degerler);
+        }
+
+        public ViewResult Siparisler()
+        {
+            var degerler = db.TBL_SIPARIS.ToList();
+            return View(degerler);
+        }
+
+        public ViewResult Stok()
+        {
+            var degerler = db.TBL_HAMMADDE.ToList();
+            return View(degerler);
+        }
+    }
+}
+
